41ebee88a8ae81326fe3bff748dbb1439f40ff06
Labonya <1aklimakter@gmail.com>
2023-11-05 16:30:13 +0600

    I have added extra section latestRecipe

---
+++ src/components/ExtraSection/NewRecipies.jsx
@@ -0,0 +1,54 @@
+
+const NewRecipies = () => {
+    return (
+        <div>
+           
+           <section className="py-6 dark:bg-gray-800 dark:text-gray-100">
+           <div className="p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-800 dark:text-gray-100">
+	<div className="flex flex-col max-w-8xl mx-auto overflow-hidden rounded">
+		<img src="https://i.ibb.co/5kqpVtx/mexican-tacos-with-beef-tomato-sauce-salsa-2829-14227.webp" alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500" />
+		<div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-5xl sm:px-10 sm:mx-12 bg-black lg:rounded-md dark:bg-gray-900">
+			<div className="space-y-2">
+            <div className="text-xl font-bold leadi text-center text-white lg:text-5xl">Our Latest Recipes</div>
+		<p className="text-center text-white">This dish offers a refreshing burst of flavors in every bite.!</p>
+			</div>
+		</div>
+	</div>
+    <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
+		<div className="flex flex-row flex-wrap-reverse justify-center">
+			
+			<div className="flex flex-col justify-center m-8 text-center">
+				<img alt="" className="self-center flex-shrink-0 w-32 h-32 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg" />
+				<p className="text-xl font-semibold leadi">Beef Wellington</p>
+				<p className="dark:text-gray-400">Beef</p>
+			</div>
+			<div className="flex flex-col justify-center m-8 text-center">
+				<img alt="" className="self-center flex-shrink-0 w-32 h-32 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg" />
+				<p className="text-xl font-semibold leadi">Broccoli Soup</p>
+				<p className="dark:text-gray-400">Starter</p>
+			</div>
+			<div className="flex flex-col justify-center m-8 text-center">
+				<img alt="" className="self-center flex-shrink-0 w-32 h-32 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://www.themealdb.com/images/media/meals/rpvptu1511641092.jpg" />
+				<p className="text-xl font-semibold leadi">Blackberry Fool</p>
+				<p className="dark:text-gray-400">Dessert</p>
+			</div>
+			<div className="flex flex-col justify-center m-8 text-center">
+				<img alt="" className="self-center flex-shrink-0 w-32 h-32 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://www.themealdb.com/images/media/meals/1548772327.jpg" />
+				<p className="text-xl font-semibold leadi">Baked salmon</p>
+				<p className="dark:text-gray-400">Seafood</p>
+			</div>
+			<div className="flex flex-col justify-center m-8 text-center">
+				<img alt="" className="self-center flex-shrink-0 w-32 h-32 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://www.themealdb.com/images/media/meals/hqaejl1695738653.jpg" />
+				<p className="text-xl font-semibold leadi">Bread</p>
+				<p className="dark:text-gray-400">Breakfast</p>
+			</div>
+		</div>
+	</div>
+</div>
+	
+</section> 
+        </div>
+    );
+};
+
+export default NewRecipies;
