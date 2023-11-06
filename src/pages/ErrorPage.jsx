9f1714b30fa941ab534fdc3c72da60e8ea7d9968
Labonya <1aklimakter@gmail.com>
2023-11-06 10:30:31 +0600

    I have added use axios custom hook

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div className=" flex justify-center text-center mt-10 lg:mt-[10%]">
+       <div>
+         <div className=" flex justify-center text-center mt-10 lg:mt-[10%]">
           <img src="https://i.ibb.co/D14prxK/404-page-dssim-837x560.jpg"  className ="rounded-lg w-96" alt="" />
+          
         </div>
+       <div className="flex justify-center mt-5">
+       <Link to="/"><button className="btn btn-outline flex justify-center">Back to Home</button></Link>
+       </div>
+       </div>
     );
 };
 
