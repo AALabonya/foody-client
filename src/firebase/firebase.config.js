0fba27709074f3212451c45290308e2bbc1156ce
Labonya <1aklimakter@gmail.com>
2023-11-06 10:53:58 +0600

    I have added the Environment variable to hide the Firebase config keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD94JA7osctkCRPl5Ziv_d1KgUfU-EmEM8",
-  authDomain: "career-hub-46ea7.firebaseapp.com",
-  projectId: "career-hub-46ea7",
-  storageBucket: "career-hub-46ea7.appspot.com",
-  messagingSenderId: "150013866457",
-  appId: "1:150013866457:web:6152b4c5db7c1dde0469c8"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
