a97d64db98978999406e312fd6e457e49bf3ed23
Labonya <1aklimakter@gmail.com>
2023-11-09 19:40:22 +0600

    I have location

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
                             title: "Good job!",
                             text: "You clicked the button!",
                             icon: "success"
-                        }) && navigate("/")
+                        }) && navigate(location?.state ? location.state : "/")
                         
                     )
                 }
